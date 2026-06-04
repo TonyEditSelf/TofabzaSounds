a73dae3ee98422931410d5d6579673849f156e24
Tony Eappen <togmotiv@gmail.com>
2026-06-04 21:56:34 +0530

    updated

--- telephony-server/src/index.js
+++ telephony-server/src/index.js
@@ -89,7 +89,3 @@ process.on("uncaughtException", (err) => {
   console.error("[server] Uncaught exception:", err);
   // Don't crash - log and continue
 });
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
